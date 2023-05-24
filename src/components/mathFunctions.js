9726c33cad2dcf9f311d9855d9155551a92453f8
michellestapp <mstapp@rocketmail.com>
2023-05-24 14:19:06 -0500

    Added all tests with logic

--- src/components/mathFunctions.js
+++ src/components/mathFunctions.js
@@ -3,17 +3,38 @@ function sum(a,b){
 return a + b;
 }
 
-export {sum};
-
-
 function divide(a,b){
 
-return 2;
+return a/b;
 }
-export {divide}; 
-
 function subtract(a,b){
 
-    return 6;
+return a-b;
+
 }
-export {subtract}
+
+function mult(a,b){
+return a*b
+}
+
+function capFirstLetter(word){
+
+    return word.charAt(0).toUpperCase() + word.slice(1) 
+}
+
+function concatenate(word1,word2){
+
+    const totalword = word1 + word2
+    return totalword
+
+}
+
+function returnSecondWord(statement){
+    const words = statement.split(' ')
+    const secondWord = words[1]
+    print(secondWord)
+return secondWord
+}
+export {sum,divide,subtract,mult,capFirstLetter,concatenate, returnSecondWord}
+
+
